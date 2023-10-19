766e9fb268e1165aa6870c7f20bf868dd400f7bb
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2023-10-19 19:00:07 +0700

    update UI

--- Assets/Scripts/ShipperImage.cs
+++ Assets/Scripts/ShipperImage.cs
@@ -4,31 +4,21 @@ using UnityEngine;
 
 public class ShipperImage : MonoBehaviour
 {
-    float delay;
+
     Vector3 previousPos;
 
-    private void Awake()
-    {
-        delay = 0;
-    }
-
-    private void Update()
+    private void LateUpdate()
     {
         transform.rotation = Quaternion.Euler(90, 0, 0);
-        if (delay >= 0.5f)
+        transform.position = transform.parent.position + new Vector3(0, 6, 6);
+        if (transform.position.x > previousPos.x)
         {
-            if (transform.position.x > previousPos.x)
-            {
-                transform.localScale = new Vector3(3, 3, 0);
-                delay = 0;
-            }
-            else if (transform.position.x < previousPos.x)
-            {
-                transform.localScale = new Vector3(-3, 3, 0);
-                delay = 0;
-            }
+            transform.localScale = new Vector3(3, 3, 0);
         }
-        delay += Time.deltaTime;
+        else if (transform.position.x < previousPos.x)
+        {
+            transform.localScale = new Vector3(-3, 3, 0);
+        }
         previousPos = transform.position;
     }
 }
